13f131330c25f3e360e21cb70dc3ee2ce5550ccf
lzb991435344 <991435344@qq.com>
2019-02-19 18:45:45 +0800

    tinyHttp
    tinyHttp代码注释

--- tinyhttp/httpd.c
+++ tinyhttp/httpd.c
@@ -34,6 +34,24 @@
 /**
 在linux下搭建教程：
    https://blog.csdn.net/yzhang6_10/article/details/51533890?_t_t_t=0.9113585329573218
+
+    工作的具体过程：
+   （1） 服务器启动，在指定端口或随机选取端口绑定 httpd 服务。
+   （2）收到一个 HTTP 请求时（其实就是 listen 的端口 accpet 的时候），派生一个线程运行 accept_request 函数。
+   （3）取出 HTTP 请求中的 method (GET 或 POST) 和 url,。对于 GET 方法，如果有携带参数，则 query_string 指针
+     指向 url 中 ？ 后面的 GET 参数。
+   （4） 格式化 url 到 path 数组，表示浏览器请求的服务器文件路径，在 tinyhttpd 中服务器文件是在 htdocs 文件夹下。
+     当 url 以 / 结尾，或 url 是个目录，则默认在 path 中加上 index.html，表示访问主页。
+   （5）如果文件路径合法，对于无参数的 GET 请求，直接输出服务器文件到浏览器，即用 HTTP 格式写到套接字上，跳到（10）。
+     其他情况（带参数 GET，POST 方式，url 为可执行文件），则调用 excute_cgi 函数执行 cgi 脚本。
+   （6）读取整个 HTTP 请求并丢弃，如果是 POST 则找出 Content-Length. 把 HTTP 200  状态码写到套接字。
+   （7） 建立两个管道，cgi_input 和 cgi_output, 并 fork 一个进程。
+   （8） 在子进程中，把 STDOUT 重定向到 cgi_outputt 的写入端，把 STDIN 重定向到 cgi_input 的读取端，关闭 cgi_input 
+     的写入端 和 cgi_output 的读取端，设置 request_method 的环境变量，GET 的话设置 query_string 的环境变量，POST 的
+     话设置 content_length 的环境变量，这些环境变量都是为了给 cgi 脚本调用，接着用 execl 运行 cgi 程序。
+   （9） 在父进程中，关闭 cgi_input 的读取端 和 cgi_output 的写入端，如果 POST 的话，把 POST 数据写入 cgi_input，
+     已被重定向到 STDIN，读取 cgi_output 的管道输出到客户端，该管道输入是 STDOUT。接着关闭所有管道，等待子进程结束
+   （10） 关闭与浏览器的连接，完成了一次 HTTP 请求与回应，因为 HTTP 是无连接的。
 */
 
 void accept_request(int);//接受请求并处理
@@ -222,10 +240,23 @@ void cat(int client, FILE *resource)
     char buf[1024];
 
     /*读取文件中的所有数据写到 socket */
+    /**
+    (1)函数：char *fgets(char *buf, int bufsize, FILE *stream);
+    (2)参数：
+    *buf: 字符型指针，指向用来存储所得数据的地址。
+    bufsize: 整型数据，指明存储数据的大小。
+    *stream: 文件结构体指针，将要读取的文件流。
+    (3)返回值：成功，则返回第一个参数buf；
+    在读字符时遇到end-of-file，则eof指示器被设置，如果还没读入任何字符就遇到这种情况，则buf保持原来的内容，返回NULL；
+    如果发生读入错误，error指示器被设置，返回NULL，buf的值可能被改变。
+    (4)作用：从文件中读入字符串
+    */
     fgets(buf, sizeof(buf), resource);
     while (!feof(resource))
     {
+        //发送数据到客户端
         send(client, buf, strlen(buf), 0);//给客户端发送数据
+        //从文件中读取到buf数组
         fgets(buf, sizeof(buf), resource);//读取文件中的数据
     }
 }
@@ -516,7 +547,7 @@ void serve_file(int client, const char *filename)
         numchars = get_line(client, buf, sizeof(buf));
 
     /*打开 sever 的文件*/
-    resource = fopen(filename, "r");
+    resource = fopen(filename, "r");//返回一个文件指针
     if (resource == NULL)
         not_found(client);
     else
